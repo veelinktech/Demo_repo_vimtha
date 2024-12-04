f7f21354c00b985cf07adb185707ab8518506eb6
veelinktech <104140334+veelinktech@users.noreply.github.com>
2024-12-04 18:02:22 +0530

    Updated by prabhu
    Hi vimtha,
    
    Please verify my code and if you want you can add this code

--- Example1.py
+++ Example1.py
@@ -1,16 +1,20 @@
+class Sample:
 
+  def add(self,a,b):
+     print(a+b)
+  def sub(self,a,b):
+     print(a-b)
 
-def add(a,b):
-  print(a+b)
-def sub(a,b):
-  print(a-b)
-def mul(a,b):
-  print(a*b)
+ def mul(self, a,b):
+      print(a*b)
+
+@staticmethod
 def div(a,b):
   print(a/b)
 
 if __name__=="__main__":
-  add(10,25)
-  sub(15,7)
-  mul(10,20)
-  div(100,25)
+  s = Sample()
+  s.add(10,25)
+  s.sub(15,7)
+  s.mul(10,20)
+  Sample.div(100,25)
